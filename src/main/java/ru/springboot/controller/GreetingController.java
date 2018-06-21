c70a881f3e7694de7dfa2019abc48b9f472ce1ed
tmvf <tmvf@yandex.ru>
2018-06-21 16:28:09 +0300

    03

--- src/main/java/ru/springboot/controller/GreetingController.java
+++ src/main/java/ru/springboot/controller/GreetingController.java
@@ -2,50 +2,49 @@ package ru.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.springboot.domain.Message;
 import ru.springboot.repositories.MessageRepository;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "новый user...") String name,
-                           Model model) {
-        model.addAttribute("greeting", "Hi " + name);
+    @GetMapping("/")
+    public String greeting() {
         return "greeting";
     }
 
-    @GetMapping
-    public String main(Model model) {
-        model.addAttribute("messages", messageRepository.findAll());
+    @GetMapping("/main")
+    public String main(Map<String, Object> map) {
+        Iterable<Message> messages = messageRepository.findAll();
+        map.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text,
                       @RequestParam String tag,
-                      Model model) {
+                      Map<String, Object> map) {
         Message message = new Message(text, tag);
         messageRepository.save(message);
-        model.addAttribute("messages", messageRepository.findAll());
+        map.put("messages", messageRepository.findAll());
         return "main";
     }
 
-    @PostMapping("filter")
+    @PostMapping("/filter")
     public String filter(@RequestParam(name = "filter") String filter,
-                         Model model) {
+                         Map<String, Object> map) {
         if (filter != null && !filter.isEmpty()) {
-            model.addAttribute("messages",
+            map.put("messages",
                     messageRepository.findByTag(filter));
         } else {
-            model.addAttribute("messages",
-                    messageRepository.findAll());
+            map.put("messages", messageRepository.findAll());
         }
         return "main";
     }
--- src/main/resources/templates/greeting.ftl
+++
@@ -1,9 +0,0 @@
-<html >
-<head>
-    <title>Title</title>
-</head>
-<body>
-<h4>${greeting}</h4>
-<h3><a href="/">List messages</a> </h3>
-</body>
-</html>
